d9df35e5547518d49a5172d1a0070e23d00cd806
Burnninel <79659103+Burnninel@users.noreply.github.com>
2023-10-21 20:14:10 -0300

    validate support login

--- assets/js/site/home/index.js
+++ assets/js/site/home/index.js
@@ -1,15 +1,61 @@
-function client() {
+function redirectToClientPage() {
     $('#client').click(function() {
         window.location.href = "http://localhost/libaryASP/client";
-    })
+    });
 }
 
-client()
+redirectToClientPage();
 
-function support() {
+function setupSupportSection() {
     $('#support').click(function() {
-        window.location.href = "http://localhost/libaryASP/support";
-    })
+        $('#template').slideUp(600, function() {
+            var supportValidationHTML = `
+                <h3 id="title">Seja bem-vindo!</h3>
+                <span id="selectionInstruction">Digite a senha para continuar:</span>
+                <div id="options">
+                    <input  id="passwordSupport" type="password" placeholder="Digite sua senha..."></input>
+                    <button class="btnOption" id="handleSupport">Entrar</button>
+                </div>
+            `;
+
+            $('#template').html(supportValidationHTML).slideDown(600);
+        });
+
+    });
 }
 
-support();
+setupSupportSection();
+
+function displayPasswordError(password) {
+    password.addClass('passwordError');
+    password.attr('placeholder', 'Senha incorreta.');
+    password.css('border-color', 'red');
+}
+
+function handlePasswordInputKeyUp(password) {
+    $(password).on('keyup', function() {
+        if(password.val().length < 1) {
+            displayPasswordError(password)
+        } else {
+            password.removeClass('passwordError');
+            password.css('border-color', '#b8afaf');
+        };
+    });
+};
+
+function handleSupportSubmit() {
+    $(document).on('click', '#handleSupport',function() {
+        let password = $('#passwordSupport');
+        
+        if(password.val() !== 'sup151617') {
+            password.val('');
+            displayPasswordError(password);
+            handlePasswordInputKeyUp(password);
+        } else {
+            password.val('');
+            window.location.href = "http://localhost/libaryASP/support";
+        };
+    });
+};
+
+handleSupportSubmit();
