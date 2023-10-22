7c24e0019b2db13b23497d565cf485e82fffcfbf
Burnninel <79659103+Burnninel@users.noreply.github.com>
2023-10-22 00:15:30 -0300

    teste css prod

--- views/site/home/index.php
+++ views/site/home/index.php
@@ -12,7 +12,7 @@
 
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 	
-  	<link rel="stylesheet" href="<?php echo $url; ?>/assets/css/home/index.css">
+  	<link rel="stylesheet" href="<?php echo $url; ?>https://libaryasp-production.up.railway.app/assets/css/home/index.css">
   	<link rel="stylesheet" href="<?php echo $url; ?>/assets/css/home/containerLogo.css">
   	<link rel="stylesheet" href="<?php echo $url; ?>/assets/css/home/containerTemplate.css">
 
