f10b5d849ff6cfdb762dbdc5ff399de6e2ccabbb
Burnninel <79659103+Burnninel@users.noreply.github.com>
2023-10-28 23:42:41 -0300

    separate css client

--- views/site/client/index.php
+++ views/site/client/index.php
@@ -13,6 +13,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
 	<link rel="stylesheet" href="<?php echo $url; ?>/assets/css/client/index.css">
+	<link rel="stylesheet" href="<?php echo $url; ?>/assets/css/client/dashboard.css">
 
 	<title>ASP Softwares</title>
 
