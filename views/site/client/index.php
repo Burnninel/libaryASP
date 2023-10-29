243572db038f1ff4c41a4d3a92d2924564346e60
Burnninel <79659103+Burnninel@users.noreply.github.com>
2023-10-29 00:09:07 -0300

    adapting come back for js

--- views/site/client/index.php
+++ views/site/client/index.php
@@ -26,12 +26,6 @@
 		<div class="container" id="containerCardGroup">
 			<div class="row" id="cardGroup"></div>
 		</div>
-		
-		<div id="comeBack">
-			<svg id="iconComeBack" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.0" stroke="currentColor">
-				<path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-			</svg>
-		</div>
 
 	</main>
 
