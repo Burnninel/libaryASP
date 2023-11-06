5e5dccc2fd3251bf74f0bf069ee913e5b4b8aac9
Burnninel <79659103+Burnninel@users.noreply.github.com>
2023-11-06 08:29:07 -0300

    Fixing Modal Phone Element Tag

--- assets/js/site/client/index.js
+++ assets/js/site/client/index.js
@@ -47,12 +47,12 @@ function createBtnChat() {
 						</svg>
 						<span class="textOptionChat">whatsapp</span>
 					</a>
-					<a class="optionDropdownChat" id="iconPhone">
+					<button class="optionDropdownChat" id="iconPhone">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000" width="20" height="20">
 							<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
 						</svg>
 						<span class="textOptionChat">(49) 3333-4444</span>
-					</a>
+					</button>
 				</div>
 			</div>
 		</div>
@@ -91,101 +91,3 @@ generateIconMenu('.cadastros', cadastros);
 generateIconMenu('.financeiro', financeiro);
 generateIconMenu('.fiscal', fiscal);
 generateIconMenu('.contabilidade', contabilidade);
-
-
-// function createBtnComeBack() {
-// 	const elementComeBack = `
-// 		<div id="comeBack">
-// 			<svg id="iconComeBack" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.0" stroke="currentColor">
-// 				<path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-// 			</svg>
-// 		</div>
-// 	`;
-
-// 	$('main').append(elementComeBack);
-// };
-
-// createBtnComeBack();
-
-// function redirectComeBack() {
-// 	$('#comeBack').click(function() {
-// 		window.location.href = 'http://localhost/libaryASP/';
-// 	});
-// };
-
-// redirectComeBack();
-
-// function generatorMenu(data) {
-
-// 	for (let item in data) {
-// 		let itemMenu = `
-// 		  <div class="card col-lg-2 col-md-3 col-sm-6" id="${item}">
-// 			<div class="iconCard">
-// 			  ${getIcon(item)}
-// 			</div>
-// 			<h3 class="titleCard">${item}</h3>
-// 		  </div>
-// 		`;
-
-// 		$('#cardGroup').append(itemMenu);
-// 	};
-
-// };
-
-// function videoDashboardGenerator(titleDashboard) {
-// 	let visualMedia = `
-// 		<div id="visualMedia">
-// 			<div id="headearClientDashboard">${titleDashboard}</div>
-// 			<div id="clientDashboard"></div>
-// 		</div>
-// 	`;
-
-// 	$('#main').append(visualMedia);
-// };
-
-// function listItemGenerator(data, menu) {
-// 	for (let item of data[menu]) {
-// 		let element = `<div class="itemDashboard" id="${item.id}">${item.name}</div>`;
-// 		$('#clientDashboard').append(element);
-// 	};
-// };
-
-// function addVideoPlayer() {
-// 	$('.itemDashboard').click(function() {
-// 		let itemVideo = $(this).attr('id');
-
-// 		console.log(itemVideo)
-
-// 		videoPlayer(itemVideo, "#clientDashboard");
-
-// 	});
-// };
-
-// function attachMenuHandlers(data) {
-// 	$('#cadastros, #financeiro, #fiscal, #contabilidade').click(function () {
-// 		let elementID = $(this).attr('id');
-
-// 		videoDashboardGenerator(elementID);
-// 		listItemGenerator(data, elementID);
-// 		$('#containerCardGroup').hide();
-
-// 		addVideoPlayer();
-// 	});
-// };
-
-// function fetchAndInitialize() {
-// 	$.ajax({
-// 		url: 'http://localhost/libaryASP/apiClient',
-// 		type: 'GET',
-// 		dataType: 'json',
-// 		success: function (data) {
-// 			generatorMenu(data);
-// 			attachMenuHandlers(data);
-// 		},
-// 		error: function (error) {
-// 			console.log(error);
-// 		}
-// 	});
-// };
-
-// fetchAndInitialize();
